70a99bd0e576634492d22f1261fec8dd75d44919
EvanYao826 <evanyao826@gmail.com>
2026-05-23 16:09:56 +0800

    fix(admin): 修复仪表盘前后端对接

--- backend/start-backend.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d C:\Users\21124\Downloads\ShopAgent-X-main\ShopAgent-X-main\backend
-set JAVA_HOME=C:\DevTools\jdk-17.0.19+10
-set M2_HOME=C:\DevTools\apache-maven-3.9.8
-set PATH=%JAVA_HOME%\bin;%M2_HOME%\bin;%PATH%
-echo Starting Spring Boot...
-mvn spring-boot:run
-pause
--- backend/test-login.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-curl -s -X POST "http://localhost:8080/api/admin/login" -H "Content-Type: application/x-www-form-urlencoded" -d "username=admin&password=admin123"
-pause
